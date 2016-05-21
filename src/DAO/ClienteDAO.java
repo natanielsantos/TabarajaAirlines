0b5c573ab99d9218349ac287d4f6bd731ae0d715
Nataniel Santos <natanielsa@gmail.com>
2016-05-20 21:19:04 -0300

    Update ClienteDAO.java
    Retirando comentários

--- src/DAO/ClienteDAO.java
+++ src/DAO/ClienteDAO.java
@@ -7,29 +7,24 @@ import controleaereo.Cliente;
 
 public class ClienteDAO {
     
-    // connection e um objeto de conexão e o que conecta com o banco de dados.
+    
     private Connection con;
     private Statement stm;
-    Conexao bancoDeDados = Conexao.getInstance(); // essa conexão esta recebendo a conexão que eu criei com o postgree na clase ConectaBD 
-                                                      // O getinstance e por que estou usando singleton 
-                                                      // ou seja so existe 1 objeto de conexão ele apenas pega a instancia.
+    Conexao bancoDeDados = Conexao.getInstance(); 
 
     public ClienteDAO() {
-        con = bancoDeDados.iniciaBanco(); // aqui eu inicio a conexão
+        con = bancoDeDados.iniciaBanco(); 
     }
 
     public void inserirNoBanco(Cliente cli) {
         try {
            
             
-            //  PreparedStatement e um objeto que recebe o comando SQL , ou seja insert ,update , select.
-            // no lugar dos dados se colocam pontos de interrogação de acordo com o tanto de dados a serem consultados ou inseridos etc etc..
+           
             PreparedStatement pst = con.prepareStatement("INSERT INTO cliente "
                     + "VALUES (?,?,?,?,?,?,?,?,?,?)");
             
-            pst.setInt(1, cli.getIdentificacao()); // cada numero significa o ponto de interrogação , o 1 e o primeiro ponto
-                                                   // o 2 o segundo ponto o 3 o terceiro e assim vai , depois da virgula voce coloca o dado que quer enviar ao banco.
-                                                   // setInt envia um inteiro setString uma String e assim por diante.
+            pst.setInt(1, cli.getIdentificacao()); 
             pst.setString(2, cli.getNome());
             pst.setString(3, cli.getLogradouro());
             pst.setInt(4, cli.getNumero());
@@ -39,10 +34,10 @@ public class ClienteDAO {
             pst.setString(8, cli.getCep());
             pst.setString(9, cli.getTelefone());
             pst.setString(10, cli.getCpf());
-            pst.executeUpdate(); // Os metodos execute , executeUpdate, ExecuteQuery execultam o PreparedStatement ou seja o comando SQL que voce configurou.
-            pst.close(); // aqui termina o comando , se o banco retornar algum erro,ele vai cair no catch e sera printado o erro na tela
+            pst.executeUpdate(); 
+            pst.close(); 
         } catch (SQLException ex) {
-            System.out.println("Erro: " + ex); // se ocorre erro ele e printado na tela.
+            System.out.println("Erro: " + ex); 
         }
     }
 
@@ -50,13 +45,12 @@ public class ClienteDAO {
         try {
             
             PreparedStatement pst = con.prepareStatement("DELETE FROM cliente WHERE idcliente = ?");
-            // o PreparedStatement recebe o PreparedStatement da conexão enviando o comando de delete
-            pst.setInt(1, cli.getIdentificacao()); // esse e o comando que coloca o dado no pontod e interrogação ou seja.
-                                                   // excluia da tabela de clientes se o idcliente for igual ao cli.getIdentificação()
-            pst.execute();  // aqui o comando e executado.
+           
+            pst.setInt(1, cli.getIdentificacao()); 
+            pst.execute();  
       
         } catch (SQLException ex) {
-            System.out.println("Erro: " + ex); // mesma coisa se ocorrer erro ele sera mostrado na tela.
+            System.out.println("Erro: " + ex);
         }
     }
 
@@ -66,8 +60,7 @@ public class ClienteDAO {
                     + "logradouro = ?,numero = ?,bairro = ?,municipio = ?,"
                     + "estado = ?,cep = ?,telefone = ?,cpf = ? WHERE idcliente = ?");
 
-            // como eu ja expliquei cada ponto de interrogação e um dado a ser enviado ao banco.
-            // cada pst.setTipo e o dado do ponto de interrogação na ordem dos numeros.
+           
             pst.setString(1, cli.getNome());
             pst.setString(2, cli.getLogradouro());
             pst.setInt(3, cli.getNumero());
@@ -87,7 +80,7 @@ public class ClienteDAO {
 
     public Cliente consultar(long cod) {
         Cliente cli;
-        ResultSet rs; // o Objeto ResultSet vai receber o resultado de uma consulta Select.
+        ResultSet rs; 
 
         try {
             PreparedStatement pst = con.prepareStatement("SELECT identificacao, nome, logradouro, numero, bairro, "
@@ -95,34 +88,30 @@ public class ClienteDAO {
                     + "FROM cliente"
                     + " WHERE identificacao = ? ");
             
-            pst.setInt(1, (int) cod); // dado do ponto de interrogação ou seja se o idcliente for igual ao dado que ta no cod.
-            rs= pst.executeQuery(); // Aqui o result set vai receber o resultado do select ou seja aquela tabelinha que aparece no postgree ta dentro desse ResultSet.
+            pst.setInt(1, (int) cod); 
+            rs= pst.executeQuery(); 
             
-            if (rs.next()) { // esse result set e igual um iterator ou seja se tiver proximo ele vai criar um cliente com os dados que estava no proximo , esse proximo e o resultado do select.
+            if (rs.next()) { 
                 cli = new Cliente(rs.getInt("identificacao"), rs.getString("nome"), rs.getString("logradouro"),
                         rs.getInt("numero"), rs.getString("bairro"), rs.getString("municipio"),
                         rs.getString("estado"), rs.getString("cep"), rs.getString("telefone"), rs.getString("cpf"));
 
-                //aqui eu to criando o cliente com os dados que foram retornados do banco.
-                //rs.getTipo pega o dado do tipo tal , que esta no indice .
-                // diferente do arraylist aqui o indice nao e do tipo inteiro ex: .get(1) , .Get(2).
-                // aqui o indice e o nome do indice da tabela do banco ou seja rs.getInt("idcliente") e o indice idcliente.
-                // ex numero = rs.getInt("idcliente"); o numero ta recebendo a id do cliente que o banco retornou.
-                return cli; // depois que eu criei o cliente com os dados que estão no banco eu retorno ele e mostro na tela os dados.
+               
+                return cli; 
             } else {
-                return null; // se nao houver cliente ele retornará null.
+                return null;
             }
 
         } catch (SQLException ex) {
-            System.out.println("Erro: " + ex); // se houver erros eles seram printados na tela.
-            return null; // se houver erro retorna null.
+            System.out.println("Erro: " + ex); 
+            return null; 
         }
     }
 
     public ArrayList relatorio() {
         ArrayList<Cliente> clientes;
         ResultSet rs;
-        // basicamente a mesma coisa do consultar porem aqui eu crio um array com todos os clientes do banco.
+       
         try {
             clientes = new ArrayList<>();
 
@@ -130,13 +119,13 @@ public class ClienteDAO {
             rs = pst.executeQuery();
        
             while (rs.next()) {
-                // enquanto tiver proximo ele coloca o proximo no arraylist.
+              
                 clientes.add(new Cliente(rs.getInt("identificacao"), rs.getString("nome"), rs.getString("logradouro"),
                         rs.getInt("numero"), rs.getString("bairro"), rs.getString("municipio"),
                         rs.getString("estado"), rs.getString("cep"), rs.getString("telefone"), rs.getString("cpf")));
             }
 
-            return clientes; // retorna o arraylist
+            return clientes; 
 
         } catch (SQLException ex) {
             System.out.println("Erro: " + ex);
