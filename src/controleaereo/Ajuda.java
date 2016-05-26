208ab0de0209db7f4932252986c6015ddc634de3
Nataniel Santos <natanielsa@gmail.com>
2016-05-25 22:07:29 -0300

    Depois a gente faz
    Depois fazemos os itens de ajuda...

--- src/controleaereo/Ajuda.java
+++ src/controleaereo/Ajuda.java
@@ -8,14 +8,6 @@ public class Ajuda {
 
 	public void ajuda() {
 
-		System.out.println("  **********======[                Ajuda               ]=====**********         ");
-		System.out.println("                         *****==[Versão 1.1]==*****                             ");
-		System.out.println("--------------------------------------------------------------------------------");
-		System.out.println("Menu Principal : ");
-		System.out.println("O Menu Principal ou Menu Gerenciador contém algumas opções para controlar a venda "
-				+ "venda de passagens áreas.");
-		System.out.println("Se você selecionar a primeira opção (1 - Cadastrar), você cadastrará um"
-				+ "cliente, avião, aeroporto, ");
 
 	}
 
